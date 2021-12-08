9091765b6542556003a3af7de2215c27425ee228
Shobhit Gupta <56156171+shobhit10058@users.noreply.github.com>
2021-12-08 20:10:42 +0530

    added mapbox for map

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
