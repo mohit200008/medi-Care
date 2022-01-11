fe2bbb10c70d9032fb28c5ae9f509d9adb9206d1
Nawed Ali <83456083+nawed2611@users.noreply.github.com>
2022-01-11 16:14:36 +0530

    Update DiseasePred Page

--- src/components/DiseasePred/DiseaseDetection.js
+++ src/components/DiseasePred/DiseaseDetection.js
@@ -10,12 +10,14 @@ function DiseaseDetection() {
           <div className="photo" style={{ backgroundImage: "url(images/BreastCancer.jfif)" }}></div>
         </div>
         <div className="description">
-          <h1>Breast cancer Disease Prediction</h1>
+          <h1>Breast Cancer Disease Prediction</h1>
           <h2>Detect breast cancer</h2>
-          <p>Symptoms of breast cancer include a lump in the breast, bloody discharge from the nipple and changes in the shape or texture of the nipple or breast.</p>
-          <button><a href="https://breastcancerdetectionweb.herokuapp.com/" target='_blank' style={{ textDecoration: "none" }}>
-            Predict</a>
-          </button>
+          <div className="para-button">
+            <p>Symptoms of breast cancer include a lump in the breast, bloody discharge from the nipple and changes in the shape or texture of the nipple or breast.</p>
+            <button><a href="https://breastcancerdetectionweb.herokuapp.com/" target='_blank' style={{ textDecoration: "none" }}>
+              Predict</a>
+            </button>
+          </div>
         </div>
       </div>
 
@@ -24,12 +26,14 @@ function DiseaseDetection() {
           <div className="photo" style={{ backgroundImage: "url(images/Diabeties.jpg)" }}></div>
         </div>
         <div className="description">
-          <h1>Diabeties prediction</h1>
+          <h1>Diabeties Prediction</h1>
           <h2>Predict the chances of getting diabeties</h2>
-          <p>Diabetes is a disease that occurs when your blood glucose, also called blood sugar, is too high. Blood glucose is your main source of energy and comes from the food you eat.</p>
-          <button><a href="https://diabetiespredictionweb.herokuapp.com/" target='_blank' style={{ textDecoration: "none" }}>
-            Predict</a>
-          </button>
+          <div className="para-button">
+            <p>Diabetes is a disease that occurs when your blood glucose, also called blood sugar, is too high. Blood glucose is your main source of energy and comes from the food you eat.</p>
+            <button><a href="https://diabetiespredictionweb.herokuapp.com/" target='_blank' style={{ textDecoration: "none" }}>
+              Predict</a>
+            </button>
+          </div>
         </div>
       </div>
 
@@ -38,12 +42,14 @@ function DiseaseDetection() {
           <div className="photo" style={{ backgroundImage: "url(images/heart.jpg)" }}></div>
         </div>
         <div className="description">
-          <h1>Heart disease</h1>
+          <h1>Heart Disease</h1>
           <h2>Predict Heart Disease</h2>
+          <div className="para-button">
           <p>It develops when the arteries that supply blood to the heart become clogged with plaque. This causes them to harden and narrow.</p>
           <button><a href="https://heartdiseasepredictionweb.herokuapp.com/" target='_blank' style={{ textDecoration: "none" }}>
             Predict</a>
           </button>
+          </div>
         </div>
       </div>
 
@@ -52,12 +58,14 @@ function DiseaseDetection() {
           <div className="photo" style={{ backgroundImage: "url(images/kidney.png)" }}></div>
         </div>
         <div className="description">
-          <h1>Kidney disease</h1>
+          <h1>Kidney Disease</h1>
           <h2>Predict kidney disease</h2>
+          <div className="para-button">
           <p>The kidneys filter waste and excess fluid from the blood. As kidneys fail, waste builds up.</p>
           <button><a href="https://kidneydiseasepredictionweb.herokuapp.com/ " target='_blank' style={{ textDecoration: "none" }}>
             Predict</a>
           </button>
+          </div>
         </div>
       </div>
     </>
