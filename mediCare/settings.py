f93b38ebb86f033a9fac0ba13960bcf5600d68d9
Ashsenior <ashutoshjunior@gmail.com>
2022-04-04 14:38:05 +0530

    herkou

--- mediCare/settings.py
+++ mediCare/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path,join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 STATICFIELS_DIRS = [
