d7370c7d6e0fd46d7116b8d7c944811bcc381d74
Ashsenior <ashutoshjunior@gmail.com>
2022-04-04 14:49:37 +0530

    herkou

--- mediCare/wsgi.py
+++ mediCare/wsgi.py
@@ -10,10 +10,7 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mediCare.settings')
 
 application = get_wsgi_application()
-
-application = DjangoWhiteNoise(application)
