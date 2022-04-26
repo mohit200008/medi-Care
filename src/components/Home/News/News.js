66de9530d95da489af8d564197ab1a12b8722e7a
Ashsenior <ashutoshjunior@gmail.com>
2022-04-26 09:01:02 +0530

    completed news section in the home page

--- src/components/Home/News/News.js
+++ src/components/Home/News/News.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
 import "../../../styles/News.css";
 import "../../../styles/w3.css";
 import img3 from "../../../assets/images/heartimg.jpg";
@@ -10,14 +10,30 @@ export default function News() {
     const [strt,setStrt] = useState(0);
     const [end,setEnd] = useState(4);
     const [total,setTotal] = useState(0);
+    const [currentTag,SetCurrentTag] = useState("heart attack");
 
+    useEffect(() => {
+        handleTag();
+    },[currentTag])
+    const PageNumbers = [];
+    const int = Math.ceil(total / 4);
+    if (int === 1 ) return null ;
+    for (let i = 1; i<= int; i++) {
+        PageNumbers.push(i) 
+    }
     // Methods 
     const handleQuery = (e) => {setQuery(e.target.value)} 
-    const handleSearch = () => {}
+    const handlePage = (e) => {
+        setStrt(e.target.value*4-4);
+        setEnd(e.target.value*4);
+    }
 
-    const handleTag = (e) => {
+    const handleCurrentTag = (e) => {SetCurrentTag(e.target.value);}
+    const handleSearch = () => { handleRequest(query); SetCurrentTag(query); } 
+    const handleTag = () => { handleRequest(currentTag); }
+    const handleRequest = (q) => {
         var url = 'https://newsapi.org/v2/everything?' +
-        'q=' + e.target.value +
+        'q=' + q +
         '&from=2022-04-24' +
         '&sortBy=popularity' +
         '&apiKey=' + API_KEY;
@@ -26,7 +42,7 @@ export default function News() {
             .then((response) => response.json())
             .then((data) => {
                 setArticles(data.articles);
-                setTotal(data.totalResults);
+                setTotal(data.articles.length);
                 console.log(data);
             });
     } 
@@ -38,28 +54,31 @@ export default function News() {
             <button onClick={handleSearch} className="search-icon w3-text-green w3-button w3-hover-green w3-large w3-round-xlarge" type="button">Search</button>
         </form>
         <div className=" w3-margin-left w3-margin-top" >
-            <button onClick={handleTag} value="heart disease" className="w3-button w3-dark-grey w3-margin-right w3-large w3-round-xlarge">Heart</button>
-            <button onClick={handleTag} value="kidney disease" className="w3-button w3-dark-grey w3-margin-right w3-large w3-round-xlarge">Kidney</button>
-            <button onClick={handleTag} value="diabetes" className="w3-button w3-dark-grey w3-margin-right w3-large w3-round-xlarge">Diabeties</button>
-            <button onClick={handleTag} value="heart" className="w3-button w3-dark-grey w3-margin-right w3-large w3-round-xlarge">India</button>
-            <button onClick={handleTag} value="corona" className="w3-button w3-dark-grey w3-margin-right w3-large w3-round-xlarge">Corona</button>
-            <button onClick={handleTag} value="breast cancer" className="w3-button w3-dark-grey w3-margin-right w3-large w3-round-xlarge">Breast Cancer</button>
+            <button onClick={handleCurrentTag} value="heart attack" className="w3-button w3-border w3-light-grey w3-margin-right w3-large w3-round-xxlarge">Heart</button>
+            <button onClick={handleCurrentTag} value="kidney disease" className="w3-button w3-border w3-light-grey w3-margin-right w3-large w3-round-xxlarge">Kidney</button>
+            <button onClick={handleCurrentTag} value="diabetes" className="w3-button w3-border w3-light-grey w3-margin-right w3-large w3-round-xxlarge">Diabeties</button>
+            <button onClick={handleCurrentTag} value="corona" className="w3-button w3-border w3-light-grey w3-margin-right w3-large w3-round-xxlarge">Corona</button>
+            <button onClick={handleCurrentTag} value="breast cancer" className="w3-button w3-border w3-light-grey w3-margin-right w3-large w3-round-xxlarge">Breast Cancer</button>
         </div>
-        <div className="w3-padding" >
+        <div className="w3-padding w3-margin-bottom w3-row" >
+        <h3 className="w3-margin">{currentTag}</h3>
         { articles ? articles.slice(strt,end).map((article) => (
-            <div class="w3-round-large w3-light-grey news-div w3-quarter">
+            <div class="w3-round-xxlarge w3-light-grey news-div w3-quarter">
                 {article.urlToImage ? <img className="w3-round-large img" src={article.urlToImage} alt="Avatar" /> : null }
                 <div class="w3-margin-top w3-padding ">
-                    <h3 className="w3-text-grey ">By {article.author} on {article.publishedAt}</h3>
-                    <p className="w3-small" >{article.description}</p>
+                    <h3 className="w3-text-black" >{article.title}</h3>
+                    <h5 className="w3-text-dark-grey ">By {article.author} on {article.publishedAt.slice(0,10)}</h5>
+                    <e className="ss" >{article.description}.. <a href={article.url} >Read More</a></e>
                     <br></br>
                     <br></br>
                 </div>
             </div>
         )) : null}
         </div>
-        <div>
-            {total ? <button></button> : null}
+        <h3 className="w3-center">Page {end/4}</h3>
+        <br></br>
+        <div className="w3-row w3-margin-bottom w3-center w3-large" >
+            {PageNumbers ? PageNumbers.map((i) => (<button value={i} onClick={handlePage} className="w3-button w3-round-xxlarge w3-dark-grey w3-margin-left" >{i}</button>)) : null}
         </div>
 
     </React.Fragment>
