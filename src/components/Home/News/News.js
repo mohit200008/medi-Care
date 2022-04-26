c50d3704073171bd0fad9c04ad02c545bc68efb4
Ashsenior <ashutoshjunior@gmail.com>
2022-04-26 23:35:41 +0530

    fixed bugs

--- src/components/Home/News/News.js
+++ src/components/Home/News/News.js
@@ -1,7 +1,6 @@
 import React, { useState , useEffect } from "react";
 import "../../../styles/News.css";
 import "../../../styles/w3.css";
-import img3 from "../../../assets/images/heartimg.jpg";
 
 export default function News() {
     const API_KEY = "4ffda4cb8a734e5193d06762fdc76dd5";
@@ -10,11 +9,11 @@ export default function News() {
     const [strt,setStrt] = useState(0);
     const [end,setEnd] = useState(4);
     const [total,setTotal] = useState(0);
-    const [currentTag,SetCurrentTag] = useState("heart attack");
+    const [currentTag,setCurrentTag] = useState("heart disease");
 
     useEffect(() => {
-        handleTag();
-    },[currentTag])
+        handleRequest(currentTag);
+    },[])
     const PageNumbers = [];
     const int = Math.ceil(total / 4);
     if (int === 1 ) return null ;
@@ -27,14 +26,13 @@ export default function News() {
         setStrt(e.target.value*4-4);
         setEnd(e.target.value*4);
     }
-
-    const handleCurrentTag = (e) => {SetCurrentTag(e.target.value);}
-    const handleSearch = () => { handleRequest(query); SetCurrentTag(query); } 
-    const handleTag = () => { handleRequest(currentTag); }
+    
+    const handleSearch = () => { handleRequest(query); setCurrentTag(query); } 
+    const handleTag = (e) => { handleRequest(e.target.value); setCurrentTag(e.target.value); }
     const handleRequest = (q) => {
         var url = 'https://newsapi.org/v2/everything?' +
         'q=' + q +
-        '&from=2022-04-24' +
+        '&searchin=title' +
         '&sortBy=popularity' +
         '&apiKey=' + API_KEY;
         var req = new Request(url);
@@ -42,33 +40,35 @@ export default function News() {
             .then((response) => response.json())
             .then((data) => {
                 setArticles(data.articles);
+                if (data.articles.length<4){console.log("less than 4");}
                 setTotal(data.articles.length);
                 console.log(data);
             });
     } 
+    
     return (
     <React.Fragment>
         <h2 className="w3-margin " >Latest News</h2>
-        <form class="w3-margin-top search-container">
+        <form className="w3-margin-top search-container">
             <input type="text" className="w3-large" id="search-bar" onChange={handleQuery} placeholder="Search for lates News related to diseases ..." />
             <button onClick={handleSearch} className="search-icon w3-text-green w3-button w3-hover-green w3-large w3-round-xlarge" type="button">Search</button>
         </form>
         <div className=" w3-margin-left w3-margin-top" >
-            <button onClick={handleCurrentTag} value="heart attack" className="w3-button w3-border w3-light-grey w3-margin-right w3-large w3-round-xxlarge">Heart</button>
-            <button onClick={handleCurrentTag} value="kidney disease" className="w3-button w3-border w3-light-grey w3-margin-right w3-large w3-round-xxlarge">Kidney</button>
-            <button onClick={handleCurrentTag} value="diabetes" className="w3-button w3-border w3-light-grey w3-margin-right w3-large w3-round-xxlarge">Diabeties</button>
-            <button onClick={handleCurrentTag} value="corona" className="w3-button w3-border w3-light-grey w3-margin-right w3-large w3-round-xxlarge">Corona</button>
-            <button onClick={handleCurrentTag} value="breast cancer" className="w3-button w3-border w3-light-grey w3-margin-right w3-large w3-round-xxlarge">Breast Cancer</button>
+            <button onClick={handleTag} value="heart disease" className="w3-button w3-border w3-light-grey w3-margin-right w3-large w3-round-xxlarge">Heart</button>
+            <button onClick={handleTag} value="kidney disease" className="w3-button w3-border w3-light-grey w3-margin-right w3-large w3-round-xxlarge">Kidney</button>
+            <button onClick={handleTag} value="diabetes" className="w3-button w3-border w3-light-grey w3-margin-right w3-large w3-round-xxlarge">Diabeties</button>
+            <button onClick={handleTag} value="corona" className="w3-button w3-border w3-light-grey w3-margin-right w3-large w3-round-xxlarge">Corona</button>
+            <button onClick={handleTag} value="breast cancer" className="w3-button w3-border w3-light-grey w3-margin-right w3-large w3-round-xxlarge">Breast Cancer</button>
         </div>
         <div className="w3-padding w3-margin-bottom w3-row" >
         <h3 className="w3-margin">{currentTag}</h3>
         { articles ? articles.slice(strt,end).map((article) => (
-            <div class="w3-round-xxlarge w3-light-grey news-div w3-quarter">
+            <div key={article.url} className="w3-round-xxlarge w3-light-grey news-div w3-quarter">
                 {article.urlToImage ? <img className="w3-round-large img" src={article.urlToImage} alt="Avatar" /> : null }
-                <div class="w3-margin-top w3-padding ">
+                <div className="w3-margin-top w3-padding ">
                     <h3 className="w3-text-black" >{article.title}</h3>
                     <h5 className="w3-text-dark-grey ">By {article.author} on {article.publishedAt.slice(0,10)}</h5>
-                    <e className="ss" >{article.description}.. <a href={article.url} >Read More</a></e>
+                    <p className="ss" >{article.description}.. <a href={article.url} >Read More</a></p>
                     <br></br>
                     <br></br>
                 </div>
@@ -78,7 +78,7 @@ export default function News() {
         <h3 className="w3-center">Page {end/4}</h3>
         <br></br>
         <div className="w3-row w3-margin-bottom w3-center w3-large" >
-            {PageNumbers ? PageNumbers.map((i) => (<button value={i} onClick={handlePage} className="w3-button w3-round-xxlarge w3-dark-grey w3-margin-left" >{i}</button>)) : null}
+            {PageNumbers[0] ? PageNumbers.map((i) => (<button value={i} onClick={handlePage} className="w3-button w3-round-xxlarge w3-dark-grey w3-margin-left" >{i}</button>)) : null}
         </div>
 
     </React.Fragment>
