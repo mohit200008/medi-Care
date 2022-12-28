a8b8bb2e7069df861130430b1a2a803bfa3d65a1
Tushar98644 <tusharbanik@Tushars-MacBook-Air.local>
2022-12-29 01:57:35 +0530

    second

--- src/components/DiseasePred/Cards/DisPredicCard.js
+++ src/components/DiseasePred/Cards/DisPredicCard.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../index.css';
+
 const DPCard = ({ img, title, subtitle, para, url }) => {
   return (
     <>
       <div className="col-md-5 col-lg-5 col-10 center mt-4">
         <img src={img} alt="Image" className="img-fluid pimg" />
-        <div className="text-wrapper">
-          <h5 className="text-center">{title}</h5>
-          <h6 className="text-center">{subtitle}</h6>
-          <p>{para}</p>
+        <div className="text-wrapper ">
+          <h5 className="text-center title">{title}</h5>
+          <h6 className="text-center sub-title">{subtitle}</h6>
+          <p className="para">{para}</p>
         </div>
 
         <a
