af07daca215bbe7358fd1b79cbcf3ea190c32b3f
Mayank77maruti <125661248+Mayank77maruti@users.noreply.github.com>
2023-12-09 12:08:07 +0530

    Footer UI Improved

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import './Footer.css'
-
+import React, { useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import './Footer.css';
 
 const Footer = () => {
-    return (
-        <div className='avi-footer-main'>
-            <hr />
-            <Container fluid className='avi-footer'>
-                <Row className='avi-footer-container'>
-                    <Col md={4} lg={4} xs={12} className="avi-contactus">
+  useEffect(() => {
+    const handleScroll = () => {
+      var footer = document.querySelector('.avi-footer');
+      var footerPosition = footer.getBoundingClientRect().top;
+      var screenPosition = window.innerHeight;
+
+      if (footerPosition < screenPosition) {
+        footer.classList.add('show-footer');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className='avi-footer-main'>
+      <hr />
+      <Container fluid className='avi-footer'>
+        <Row className='avi-footer-container'>
+           <Col md={4} lg={4} xs={12} className="avi-contactus">
                         <h1>Contact Us</h1> <br />
                         <p className="avi-footer-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi amet quis ullam. Veniam quam quis iure quae dicta, sint ab?</p>
                         <a href="tel:+91 9874561230" className='avi-contact-link'><i className='fa fa-phone'></i>&emsp;+3434-837-4634</a>
@@ -36,19 +53,19 @@ const Footer = () => {
                             <a href="#"><i class="fa-brands fa-linkedin-in fa-2x p-3"></i></a>
                         </div>
                     </Col>
-                </Row>
-            </Container>
-            <hr />
-            <div className="avi-copyright-sec">
-                <p>
-                    @2022 Copyright by
-                    <a href="#">Medicare</a>
-                    <p>By Mohit Lamba</p>
-                    All Rights Reserved.
-                </p>
-            </div>
-        </div>
-    )
-}
+        </Row>
+      </Container>
+      <hr />
+      <div className="avi-copyright-sec">
+        <p>
+          @2022 Copyright by
+          <a href="#">Medicare</a>
+          <p>By Mohit Lamba</p>
+          All Rights Reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
